213ca5c7ad158b75abf6c747152a5e3b8cd6923e
TCSA\pmesparza <pmesparza@itracasa.es>
2025-03-12 13:29:44 +0100

    changes to kafka

---
+++ database_create_pamplona.sql
@@ -0,0 +1,12 @@
+ CREATE TABLE pamplona.iot (
+	recvtime text NULL,
+	fiwareservicepath text NULL,
+	entityid text NULL,
+	entitytype text NULL,
+	t text NULL,
+	t_md text NULL,
+  h text NULL,
+  h_md text NULL,
+	timeinstant text NULL,
+	timeinstant_md text NULL
+);
