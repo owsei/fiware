ce8c27f71ad674f9706a5c956d7eb37acfe375d6
Pedro <owsei@yahoo.com>
2026-05-12 07:37:01 +0200

    changes to keycloak security

---
+++ timescale/init-keycloak.sql
@@ -0,0 +1,7 @@
+
+-- create schema if not exists keycloak;
+
+-- crea la base de datos y el esquema keycloak, y se conecta a la DB para crear el schema
+CREATE DATABASE keycloak OWNER admin;
+\connect keycloak
+CREATE SCHEMA IF NOT EXISTS keycloak AUTHORIZATION admin;
